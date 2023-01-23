5680416bd29460635d9d4e2d5aa8a869578345fe
Ben <git@othyn.com>
2023-01-23 21:14:15 +0000

    Version bump

--- components/sections/home/Version.vue
+++ components/sections/home/Version.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
     <p>
-      Version:
       <a
         href="https://github.com/othyn/othyn.com/releases"
         target="_blank"
         rel="noopener noreferrer"
-        >1.1.1</a
+        >v1.2.5</a
       >
       <a class="pull-right" href="#top">Top</a>
     </p>
